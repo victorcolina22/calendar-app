b07ac4296852018fbbc1d835a9037042a85b9be6
Victor Colina <victorcolina2202@gmail.com>
2022-03-21 16:17:55 -0300

    React Big Calendar(Calendar)

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import { Navbar } from '../ui/Navbar';
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+const localizer = momentLocalizer(moment);
+
+const myEventsList = [
+    {
+        title: 'Cumpleaños de vero',
+        start: moment().toDate(),
+        end: moment().add(2, 'hours').toDate()
+    }
+];
 
 export const CalendarScreen = () => {
     return (
-        <Navbar />
+        <>
+            <Navbar />
+
+            <div className="myCustomHeight calendar-screen">
+                <Calendar
+                    localizer={localizer}
+                    events={myEventsList}
+                    startAccessor="start"
+                    endAccessor="end"
+                />
+            </div>
+        </>
     )
 }
